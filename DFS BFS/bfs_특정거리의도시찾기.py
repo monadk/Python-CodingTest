75a1c05dbacea59a9bef828ebaf758e3eabe013e
Jade Kim <jadek.engr@gmail.com>
2021-10-19 22:12:39 +0900

    bfs

--- DFS BFS/bfs_특정거리의도시찾기.py
+++ DFS BFS/bfs_특정거리의도시찾기.py
@@ -1,5 +1,18 @@
+from collections import deque
 
 n, m, k, x = map(int, input().split())
 roads = []
 for _ in range(m):
     roads.append(tuple(map(int, input().split())))
+
+cities = [0]*(n+1)
+
+
+def bfs(start):
+    # blabla
+    q = deque(start)
+
+
+bfs(x)
+
+print(i for i in range(1, n+1) if cities(i) == k)
